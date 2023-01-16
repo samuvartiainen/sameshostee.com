d90b872325ff34ccd3462fc19549fbff3597440b
samuvartiainen <44369958+samuvartiainen@users.noreply.github.com>
2023-01-16 12:56:19 +0200

    2/Added material ui + mui carousel

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -36,9 +36,7 @@ export const Header = () => {
         </div>
       </div>
       <div className="header__banner-content">
-        <img
-          src="logo512.png"
-        />
+
       </div>
     </header>
   )
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,3 +1,5 @@
+import { Button, Paper } from "@mui/material"
+import Carousel from "react-material-ui-carousel"
 import { Link } from "../Link"
 import "./Projects.css"
 export const Projects = ({
@@ -5,14 +7,53 @@ export const Projects = ({
   href,
   ...props
 }) => {
+
+  const items = [
+    {
+        name: "Trains search",
+        description: "Web app for searching trains in Finland. You can see the departing and arriving trains for each station. Using API by Digitraffic."
+    },
+    {
+        name: "Thesis",
+        description: "Hello World!"
+    }
+]
+
+
   return (
     <div className="projects">
       <div className="projects__text-container">
         <h1>My projects</h1>
       </div>
-      <div className="projects__content-container">
-        <Link href="junanhakusivu">open</Link>
-      </div>
+      <Carousel
+      indicatorContainerProps={{
+        style: {
+            marginTop: '50px', // 5
+            background: "lightblue",
+        }
+      }}
+      
+      autoPlay={false} interval={10000}>
+      {
+        items.map( (item, i) => <Item key={i} item={item} /> )
+      }
+      </Carousel>
     </div>
   )
+}
+
+function Item(props)
+{
+    return (
+        <Paper className="projects__example">
+          <h2>{props.item.name}</h2>
+          <p>{props.item.description}</p>
+          <img
+            src="logo192.png"
+          />
+          <Button className="CheckButton">
+              Open
+          </Button>
+        </Paper>
+    )
 }
