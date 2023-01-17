2de283ae9fb149705a1678348085515abd6a9d2a
samuvartiainen <44369958+samuvartiainen@users.noreply.github.com>
2023-01-17 11:22:27 +0200

    2/Social links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,32 @@
+import { Button } from "../Button"
+import { Link } from "../Link"
+import { GithubSvg } from "../svgs/GithubSvg"
+import { LinkedinSvg } from "../svgs/LinkedinSvg"
 import "./Footer.css"
 
 export const Footer = () => {
   return (
     <footer className="footer">
-      
+      <Link
+        className="footer__github-link"
+        key="github"
+        href="https://github.com/samuvartiainen"
+        target="_blank"
+      >
+        <GithubSvg
+          className="footer__github-svg"
+        />
+      </Link>
+      <Link
+        className="footer__linkedin-link"
+        key="linkedin"
+        href="https://www.linkedin.com/in/samu-vartiainen/"
+        target="_blank"
+      >
+        <LinkedinSvg
+          className="footer__linkedin-svg"
+        />
+      </Link>
     </footer>
   )
 }
